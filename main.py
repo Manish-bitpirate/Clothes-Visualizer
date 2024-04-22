d73aaed94f2a7bbab9a50565efce990ab9c579d2
Manish <md663744@gmail.com>
2024-04-22 11:40:33 +0530

    added mediapipe posedetector model in webcam feed

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import cv2
 from cvzone.PoseModule import PoseDetector
 
 cap = cv2.VideoCapture(0)
+detector = PoseDetector()
 
-while cap.isOpened():
+while True:
     success, img = cap.read()
-    cv2.imshow('Mediapipe Feed', img)
+    img = detector.findPose(img)
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False)
+    if bboxInfo:
+        center = bboxInfo["center"]
+        cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+
+    cv2.imshow("Live", img)
     
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
