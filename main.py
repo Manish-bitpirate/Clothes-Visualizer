9ecb503046552c2c6cb5e8adc8026dedd574338d
Manish <md663744@gmail.com>
2024-04-22 13:50:45 +0530

    overlayed shirt over user in live feed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import cvzone
 import cv2
 from cvzone.PoseModule import PoseDetector
@@ -7,6 +8,9 @@ cap = cv2.VideoCapture(0)
 # adds PoseDetector to the feed
 detector = PoseDetector()
 
+#import shirts
+shirts = "assets/img"
+listShirts = os.listdir(shirts)
 
 #live camera feed
 while True:
@@ -19,9 +23,29 @@ while True:
     
     #if anything in bounding box or landmark list detected
     if lmList:
-        pass
-    
-    #feed name
+        #extracting torso points
+        lm12 = lmList[12][1:3]
+        
+        #join and use imported shirts path in feed
+        shirtImg = cv2.imread(os.path.join(shirts, listShirts[0]), cv2.IMREAD_UNCHANGED)
+        
+        #debug
+        # cv2.imshow("Image", shirtImg)
+      
+# defective
+        #Overlay the shirts over the user in the feed
+        try:
+            img = cvzone.overlayPNG(img, shirtImg, lm12)
+            # img = cv2.resize(shirtImg, (0,0), None, 0.5, 0.5)
+            # img = cv2.resize(shirtImg, (0,0), fx=0.4, fy=0.4) 
+            print("Try")
+        except:
+            print("except")
+            pass
+        else:
+            print("Working perfectly")
+            
+    # feed name
     cv2.imshow("Live", img)
     
     #exit statement
