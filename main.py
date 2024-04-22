f157e8dc386cf41b48cb8fa4206cb8b76880c400
Manish <md663744@gmail.com>
2024-04-22 11:48:46 +0530

    documention of current config

--- main.py
+++ main.py
@@ -2,21 +2,30 @@ import cvzone
 import cv2
 from cvzone.PoseModule import PoseDetector
 
+# camera feed
 cap = cv2.VideoCapture(0)
+# adds PoseDetector to the feed
 detector = PoseDetector()
 
-while True:
-    success, img = cap.read()
-    img = detector.findPose(img)
-    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False)
-    if bboxInfo:
-        center = bboxInfo["center"]
-        cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
 
+#live camera feed
+while True:
+    #reads the feed 
+    success, img = cap.read()
+    # and applies landmark points
+    img = detector.findPose(img)
+    #gets landmark list and bounding box info
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
+    
+    #if anything in bounding box or landmark list detected
+    if lmList:
+        pass
+    
+    #feed name
     cv2.imshow("Live", img)
     
+    #exit statement
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
-        
 cap.release()
 cv2.destroyAllWindows()
