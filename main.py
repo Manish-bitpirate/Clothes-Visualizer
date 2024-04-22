5229ca0dba8d354a8ca880193a40bfae0b19dcb7
Manish <md663744@gmail.com>
2024-04-22 11:37:27 +0530

    initial web cam setup

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello ")
+import cvzone
+import cv2
+from cvzone.PoseModule import PoseDetector
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, img = cap.read()
+    cv2.imshow('Mediapipe Feed', img)
+    
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
